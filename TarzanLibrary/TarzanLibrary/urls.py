ef43e25413f5c4c84741cda5c882a82dbf97b887
samarthhegdekalgar <samarthhegdekalgar@gmail.com>
2019-10-18 22:45:49 +0530

    Tarzan project upated

--- TarzanLibrary/TarzanLibrary/urls.py
+++ TarzanLibrary/TarzanLibrary/urls.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 admin.site.site_header = 'Tarzan Skills'
 admin.site.index_title = 'Library Administrator'
 admin.site.site_title = 'TarzanSkills Reference Center'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('LibraryManagement.urls'))
+    path(r'^admin/', admin.site.urls),
+    path('', include('LibraryManagement.urls')),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
